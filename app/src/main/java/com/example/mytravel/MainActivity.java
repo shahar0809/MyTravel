f512291b495e5095041b5c5ee7b1a99b946ad130
Shahar Tefler <shahartefler@gmail.com>
2021-02-23 20:12:40 +0200

    Update login/signup screen

--- app/src/main/java/com/example/mytravel/MainActivity.java
+++ app/src/main/java/com/example/mytravel/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.mytravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Opening login and sign up screen
+        Intent intent = new Intent(this, LoginAndSignup.class);
+        startActivity(intent);
     }
 }
