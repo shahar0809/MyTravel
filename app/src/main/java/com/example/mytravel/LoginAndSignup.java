420b4c649841999668b2b387ecda9888935afcb3
Shahar Tefler <shahartefler@gmail.com>
2021-02-23 19:20:51 +0200

    Add login/signup activity

---
+++ app/src/main/java/com/example/mytravel/LoginAndSignup.java
@@ -0,0 +1,14 @@
+package com.example.mytravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAndSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_and_signup);
+    }
+}
